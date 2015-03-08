958a63329df012d623a3bce61d24c2817c7c0343
gulraizshabbir <gulraiz.shabbir@gmail.com>
2015-03-09 02:30:24 +0500

    Assignment1_Exdata-012

--- plot1.R
+++ plot1.R
@@ -7,6 +7,8 @@ data<-read.table("household_power_consumption.txt",sep=";",header=TRUE, na.strin
 cran<-tbl_df(data)
 mehvesh<-filter(cran,Date=="1/2/2007"|Date=="2/2/2007")
 mehvi<-mutate(mehvesh,datentime=dmy(Date)+hms(Time))
+par(mar=c(4,4,2,1),adj=0.5,cex=0.8)
 hist(mehvi$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency",main="Global Active Power")
 dev.copy(png,file="plot1.png")
+png(filename="plot1.png", width=480, height=480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -9,8 +9,9 @@ mehvesh<-filter(cran,Date=="1/2/2007"|Date=="2/2/2007")
 mehvi<-mutate(mehvesh,datentime=dmy(Date)+hms(Time))
 x<-as.POSIXlt(mehvi$datentime)
 rabia<-cbind(mehvi,x)
-
+par(mar=c(4,4,4,4),adj=0.5,cex=0.8)
 
 with(rabia,plot(x,Global_active_power,ylab="Global Active Power(kilowatts)",xlab="",type="l"))
 dev.copy(png,file="plot2.png")
+png(filename="plot2.png", width=480, height=480)
 dev.off()
